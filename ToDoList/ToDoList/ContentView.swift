d1987a69d8e80ec7c1a3dc4d54b10847d9355a12
Hayden Hastings <haydenhastings32@gmail.com>
2019-10-11 09:23:41 -0600

    Initial commit.

---
+++ ToDoList/ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDoList
+//
+//  Created by Hayden Hastings on 10/11/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
